c9bcfed3af7ac1119ad8b6317e5476117043e368
BoleLee <964624188@qq.com>
2018-08-14 16:19:14 +0800

    initial commits

---
+++ README.md
@@ -0,0 +1,9 @@
+
+```
+npm install wepy-cli -g
+
+npm install
+npm run dev
+
+npm run build
+```
---
+++ wepy.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const LessPluginAutoPrefix = require('less-plugin-autoprefix');
+var prod = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  wpyExt: '.wpy',
+  eslint: true,
+  cliLogs: !prod,
+  build: {
+  },
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, 'src')
+    },
+    aliasFields: ['wepy', 'weapp'],
+    modules: ['node_modules']
+  },
+  compilers: {
+    less: {
+      compress: prod,
+      plugins: [
+        new LessPluginAutoPrefix({
+          browsers: ['Android >= 2.3', 'Chrome > 20', 'iOS >= 6']
+        })
+      ]
+    },
+    /*sass: {
+      outputStyle: 'compressed'
+    },*/
+    babel: {
+      sourceMap: true,
+      presets: [
+        'env'
+      ],
+      plugins: [
+        'transform-class-properties',
+        'transform-decorators-legacy',
+        'transform-object-rest-spread',
+        'transform-export-extensions',
+      ]
+    }
+  },
+  plugins: {
+  },
+  appConfig: {
+    noPromiseAPI: ['createSelectorQuery']
+  }
+}
+
+if (prod) {
+
+  // 压缩sass
+  // module.exports.compilers['sass'] = {outputStyle: 'compressed'}
+
+  // 压缩js
+  module.exports.plugins = {
+    uglifyjs: {
+      filter: /\.js$/,
+      config: {
+      }
+    },
+    imagemin: {
+      filter: /\.(jpg|png|jpeg)$/,
+      config: {
+        jpg: {
+          quality: 80
+        },
+        png: {
+          quality: 80
+        }
+      }
+    }
+  }
+}
